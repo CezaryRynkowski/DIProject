2bb84245d09e9079c45f970d2a9e856ca918056a
Cezary Rynkowski <crynkowski@cludo.pl>
2017-11-06 12:41:23 +0100

    Move logic from controller to separate service

--- DIProject/Controllers/DIPController.cs
+++ DIProject/Controllers/DIPController.cs
@@ -3,12 +3,19 @@ using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Web.Mvc;
+using DIProject.Services;
 
 namespace DIProject.Controllers
 {
     public class DIPController : Controller
     {
         private static Image _image;
+        private ImageProcessingService _imageProcessing;
+
+        public DIPController(ImageProcessingService imageProcessing)
+        {
+            _imageProcessing = imageProcessing;
+        }
 
         [HttpGet]
         public ActionResult Index()
@@ -36,67 +43,25 @@ namespace DIProject.Controllers
         [HttpPost]
         public ActionResult Invert()
         {
-            var bmap = (Bitmap)_image;
-            for (var i = 0; i < bmap.Width; i++)
-            {
-                for (var j = 0; j < bmap.Height; j++)
-                {
-                    var c = bmap.GetPixel(i, j);
-                    bmap.SetPixel(i, j, Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B));
-                }
-            }
-            return ReturnBitmap(bmap);
+            return ReturnBitmap(ImageProcessingService.Invert((Bitmap)_image), false);
         }
 
         [HttpPost]
         public ActionResult SetGrayscale()
         {
-            var bmap = (Bitmap)_image;
-            for (var i = 0; i < bmap.Width; i++)
-            {
-                for (var j = 0; j < bmap.Height; j++)
-                {
-                    var c = bmap.GetPixel(i, j);
-                    var gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
-                    bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
-                }
-            }
-            return ReturnBitmap(bmap);
+            return ReturnBitmap(ImageProcessingService.SetGrayscale((Bitmap)_image), false);
         }
 
+        [HttpPost]
         public ActionResult SetBrightness(int brightness)
         {
-            Bitmap bmap = (Bitmap)_image;
-            if (brightness < -255) brightness = -255;
-            if (brightness > 255) brightness = 255;
-            Color c;
-            for (int i = 0; i < bmap.Width; i++)
-            {
-                for (int j = 0; j < bmap.Height; j++)
-                {
-                    c = bmap.GetPixel(i, j);
-                    int cR = c.R + brightness;
-                    int cG = c.G + brightness;
-                    int cB = c.B + brightness;
-
-                    if (cR < 0) cR = 1;
-                    if (cR > 255) cR = 255;
-
-                    if (cG < 0) cG = 1;
-                    if (cG > 255) cG = 255;
-
-                    if (cB < 0) cB = 1;
-                    if (cB > 255) cB = 255;
-
-                    bmap.SetPixel(i, j, Color.FromArgb((byte)cR, (byte)cG, (byte)cB));
-                }
-            }
-            return ReturnBitmap(bmap);
+            return ReturnBitmap(ImageProcessingService.SetBrightness((Bitmap)_image, brightness), false);
         }
 
-        private ContentResult ReturnBitmap(Image bitmap)
+        private ContentResult ReturnBitmap(Image bitmap, bool overrideCurrentImage)
         {
-            _image = bitmap;
+            if (overrideCurrentImage)
+                _image = bitmap;
             var imageBytes = ImageToByteArray(bitmap);
             var base64 = ImageBytesToBase64(imageBytes);
             return Content(base64);
---
+++ DIProject/Services/ImageProcessingService.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+
+namespace DIProject.Services
+{
+    public abstract class ImageProcessingService
+    {
+        public static Bitmap Invert(Bitmap image)
+        {
+            for (var i = 0; i < image.Width; i++)
+            {
+                for (var j = 0; j < image.Height; j++)
+                {
+                    var c = image.GetPixel(i, j);
+                    image.SetPixel(i, j, Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B));
+                }
+            }
+            return image;
+        }
+
+        public static Bitmap SetGrayscale(Bitmap image)
+        {
+            for (var i = 0; i < image.Width; i++)
+            {
+                for (var j = 0; j < image.Height; j++)
+                {
+                    var c = image.GetPixel(i, j);
+                    var gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
+                    image.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            return image;
+        }
+
+        public static Bitmap SetBrightness(Bitmap image, int brightness)
+        {
+            if (brightness < -255) brightness = -255;
+            if (brightness > 255) brightness = 255;
+            Color c;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    c = image.GetPixel(i, j);
+                    int cR = c.R + brightness;
+                    int cG = c.G + brightness;
+                    int cB = c.B + brightness;
+
+                    if (cR < 0) cR = 1;
+                    if (cR > 255) cR = 255;
+
+                    if (cG < 0) cG = 1;
+                    if (cG > 255) cG = 255;
+
+                    if (cB < 0) cB = 1;
+                    if (cB > 255) cB = 255;
+
+                    image.SetPixel(i, j, Color.FromArgb((byte)cR, (byte)cG, (byte)cB));
+                }
+            }
+            return image;
+        }
+    }
+}
